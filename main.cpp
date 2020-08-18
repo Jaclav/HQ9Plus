2354aa289c55c2f119b71cffcace7dfeaf4e116f
Jacek <kancelariajkw@gmail.com>
2020-08-18 22:25:45 +0200

    Dodanie 9 i inne
    Dodano obsługę 9 = 99 bottles of beer, funkcje są funcjami a nie inline

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char** argv){
 		return 2;
 	}
 
-	//start .asm
+	//asm header
 	tmp<<"section .text\n";
 	tmp<<"global _start\n";
 	tmp<<"_start:\n";
@@ -32,13 +32,12 @@ int main(int argc, char** argv){
 	while(getline(file,line)){
 		for(int x=0;x<line.size();x++){
 			if(line[x]=='h'){
-				tmp<<"mov rax, 1\n";
-				tmp<<"mov rdi, 1\n";
-				tmp<<"mov rsi, hello\n";
-				tmp<<"mov rdx, helloL\n";
-				tmp<<"syscall\n";
+				tmp<<"call H\n";
 			}
-			else(line[x]=='+'){
+			else if(line[x]=='9'){
+				tmp<<"call nine\n";
+			}
+			else{
 				tmp<<"inc rax\n";
 			}
 		}
@@ -48,10 +47,42 @@ int main(int argc, char** argv){
 	tmp<<"mov rdi, 0\n";
 	tmp<<"syscall\n";
 
+	tmp<<"H:\n";
+	tmp<<"mov rax, 1\n";
+	tmp<<"mov rdi, 1\n";
+	tmp<<"mov rsi, hello\n";
+	tmp<<"mov rdx, helloL\n";
+	tmp<<"syscall\n";
+	tmp<<"ret\n";
+
+	tmp<<"nine:\n";
+	tmp<<"mov edx, str_len\n";
+    tmp<<"mov ecx, str\n";
+    tmp<<"mov ebx, 1\n";
+    tmp<<"mov eax, 4\n";
+    tmp<<"int 0x80\n";
+	tmp<<"ret\n";
+
 	//dane
 	tmp<<"segment .data\n";
 	tmp<<"hello db \"Hello World!\",0x0a,0x00\n";
-	tmp<<"helloL equ $-tekst";
+	tmp<<"helloL equ $-hello\n";
+	tmp<<"str:\n";
+	tmp<<"%assign bottles 99\n";
+	tmp<<"%rep 99\n";
+	tmp<<"%defstr bottles_str bottles\n";
+	tmp<<"%if bottles == 1\n";
+	tmp<<"%define bottle_plur \" bottle\"\n";
+	tmp<<"%else\n";
+	tmp<<"%define bottle_plur \" bottles\"\n";
+	tmp<<"%endif\n";
+	tmp<<"db bottles_str, bottle_plur, \" of beer on the wall\", 10\n";
+	tmp<<"db bottles_str, bottle_plur, \" of beer\", 10\n";
+	tmp<<"db \"Take one down, pass it around\", 10, 10\n";
+	tmp<<"%assign bottles bottles-1\n";
+	tmp<<"%endrep\n";
+	tmp<<"db \"0 bottles of beer on the wall\", 10\n";
+	tmp<<"str_len: equ $ - str\n";
 
 	file.close();
 	tmp.close();
