739389cb789f84ae1b77a74b9c9b87562684d2ab
Jacek <kancelariajkw@gmail.com>
2020-08-19 22:12:46 +0200

    Optymalizacja i poprawa kodu
    Komendy mają osobne funkcje które są w hash mapie<char>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 run: main.cpp main.hq
-	g++ main.cpp -o HQ9+
-	./HQ9+ main.hq
+	g++ -std=c++2a main.cpp -o hq9+
+	./hq9+ main.hq
 clean:
 	rm /tmp/main.asm
 	rm /tmp/main.o
--- main.cpp
+++ main.cpp
@@ -2,21 +2,38 @@
 #include <fstream>
 #include <cstring>
 #include <cstdlib>
+#include <map>
 using namespace std;
 
+enum Options{compile=0,assembler=1,object=2};
+typedef void (*Command)(std::fstream&);
+
 fstream file;
-fstream asembled;
+fstream assembled;
 string line;
 char* temp;
-
-enum Options{compile=0,assembler=1,object=2};
 Options options=compile;
+map<char,Command>commands;
+
+void cmdH(std::fstream&);
+void cmdQ(std::fstream&);
+void cmd9(std::fstream&);
+void cmdP(std::fstream&);
 
 int main(int argc, char** argv){
+	if(system("which nasm > /dev/null")){
+		std::cerr<<"hq9+: nasm compiler not found!\n";
+		exit(1);
+	}
+	if(system("which ld > /dev/null")){
+		std::cerr<<"hq9+: ld linker not found!\n";
+		exit(1);
+	}
+
 	if(argc==1){//is any parameter
-		std:cerr<<"hq9+: no input file specifed\n";
+		std::cerr<<"hq9+: no input file specifed\n";
 		cout<<"type 'hq9+ -h' for help\n";
-		return 1;
+		return 2;
 	}
 	if(strcmp(argv[1],"-h")==0||strcmp(argv[1],"-help")==0||strcmp(argv[1],"--help")==0){//help
 		cout<<"usage: hq9+ [-h] filename\n";
@@ -27,35 +44,29 @@ int main(int argc, char** argv){
 	}
 
 	file.open(argv[1],ios::in);
-	asembled.open("/tmp/main.asm",ios::out);
+	assembled.open("/tmp/main.asm",ios::out);
 	if(!file.good()){
-		return 2;
+		return 3;
 	}
+	commands['h']=cmdH;
+	commands['H']=cmdH;
+
+	commands['q']=cmdQ;
+	commands['Q']=cmdQ;
+
+	commands['9']=cmd9;
+
+	commands['+']=cmdP;
 
 	//asm header
-	asembled<<"section .text\n";
-	asembled<<"global _start\n";
-	asembled<<"_start:\n";
+	assembled<<"section .text\n";
+	assembled<<"global _start\n";
+	assembled<<"_start:\n";
 
 	while(getline(file,line)){
 		for(int x=0;x<line.size();x++){
-			if(line[x]=='h'||line[x]=='H'){
-				asembled<<"mov rdi, h_str\n";
-				asembled<<"mov rsi, h_str_len\n";
-				asembled<<"call print\n";
-			}
-			else if(line[x]=='q'||line[x]=='Q'){
-				asembled<<"mov rdi, q_str\n";
-				asembled<<"mov rsi, q_str_len\n";
-				asembled<<"call print\n";
-			}
-			else if(line[x]=='9'){
-				asembled<<"mov rdi, nnbob\n";
-				asembled<<"mov rsi, nnbob_len\n";
-				asembled<<"call print\n";
-			}
-			else if(line[x]=='+'){
-				asembled<<"inc rax\n";
+			if(commands.contains(line[x])){
+				commands[line[x]](assembled);
 			}
 			else{
 				cerr<<"'"<<line[x]<<"' is unknown command!";
@@ -64,45 +75,48 @@ int main(int argc, char** argv){
 		}
 	}
 	//wyjście z programu
-	asembled<<"mov rax, 60\n";
-	asembled<<"mov rdi, 0\n";
-	asembled<<"syscall\n";
+	assembled<<"mov rax, 60\n";
+	assembled<<"mov rdi, 0\n";
+	assembled<<"syscall\n";
 
-	asembled<<"print:\n";
-	asembled<<"mov rdx, rsi\n";
-	asembled<<"mov rsi, rdi\n";
-	asembled<<"mov rax, 1\n";
-	asembled<<"mov rdi, 1\n";
-	asembled<<"syscall\n";
-	asembled<<"ret\n";
+	assembled<<"print:\n";
+	assembled<<"mov rdx, rsi\n";
+	assembled<<"mov rsi, rdi\n";
+	assembled<<"mov rax, 1\n";
+	assembled<<"mov rdi, 1\n";
+	assembled<<"syscall\n";
+	assembled<<"ret\n";
 
 	//dane
-	asembled<<"segment .data\n";
+	assembled<<"segment .data\n";
 
 	//h
-	asembled<<"h_str db \"Hello World!\",0x0a,0x00\n";
-	asembled<<"h_str_len equ $-h_str\n";
+	assembled<<"h_str db \"Hello World!\",0x0a,0x00\n";
+	assembled<<"h_str_len equ $-h_str\n";
 
 	//q
 	file.close();
 	file.open(argv[1],ios::in);
 
-	asembled<<"q_str db \"";
+	assembled<<"q_str db \"";
 	while(getline(file,line)){
-		asembled<<line<<"\","<<0x0a;
+		assembled<<line<<"\","<<0x0a;
 	}
 	file.close();
-	asembled<<"\n";
-	asembled<<"q_str_len equ $-q_str\n";
+	assembled<<"\n";
+	assembled<<"q_str_len equ $-q_str\n";
 
 	//9
-	asembled<<"nnbob:";
+	assembled<<"nnbob:";
 	for(int i=99;i>1;i--){
-			asembled<<"db \""<<i<<"bottles of beer on the wall, "<<i<<" bottles of beer. Take one down and pass it around - "<<i-1<<"bottles of beer on the wall.\",0x0a\n";
+			assembled<<"db \""<<i<<"bottles of beer on the wall, "<<i<<" bottles of beer. Take one down and pass it around - "<<i-1<<"bottles of beer on the wall.\",0x0a\n";
     }
-    asembled<<"db \"1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around - no more bottles of beer on the wall.\",0x0a,0x00\n";
-	asembled<<"nnbob_len: equ $ - nnbob\n";
-	asembled.close();
+    assembled<<"db \"1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around - no more bottles of beer on the wall.\",0x0a,0x00\n";
+	assembled<<"nnbob_len: equ $ - nnbob\n";
+
+	//+
+	assembled<<"accumulator dq 0\n";
+	assembled.close();
 
 	if(options==compile){
 		system("nasm -f elf64 /tmp/main.asm");
@@ -118,4 +132,25 @@ int main(int argc, char** argv){
 	}
 
 	return 0;
+}
+
+void cmdH(std::fstream &assembled){
+	assembled<<"mov rdi, h_str\n";
+	assembled<<"mov rsi, h_str_len\n";
+	assembled<<"call print\n";
+}
+void cmdQ(std::fstream &assembled){
+	assembled<<"mov rdi, q_str\n";
+	assembled<<"mov rsi, q_str_len\n";
+	assembled<<"call print\n";
+}
+void cmd9(std::fstream &assembled){
+	assembled<<"mov rdi, nnbob\n";
+	assembled<<"mov rsi, nnbob_len\n";
+	assembled<<"call print\n";
+}
+void cmdP(std::fstream &assembled){
+	assembled<<"mov rax, [accumulator]\n";
+	assembled<<"inc rax\n";
+	assembled<<"mov [accumulator], rax\n";
 }
